2a90760056e677d8dfcda134ea35a741d8acb87c
Nicolopez93 <nicolopezarq@gmail.com>
2024-02-29 13:40:47 -0300

    arregle el problema del buscador

--- vite-project/src/components/pages/home/Home.jsx
+++ vite-project/src/components/pages/home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     const [autos, setAutos] = useState([])
     const [dispatchLike, setDispatchLike] = useState(false)
     const [searchResults, setSearchResults] = useState([]);
-    const [shuffledAutos, setShuffledAutos] = useState([]);
+
 
     useEffect(() => {
         axios.get("http://localhost:5000/autos")
@@ -27,56 +27,34 @@ const Home = () => {
          .catch(err => console.log(err))
         
         }
-        const shuffleArray = (array) => {
-            const shuffledArray = array.slice(); // Clonar el arreglo para no modificar el original
-            for (let i = shuffledArray.length - 1; i > 0; i--) {
-              const j = Math.floor(Math.random() * (i + 1));
-              [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-            }
-            return shuffledArray;
-          };
+        const handleSearch = (searchTerm) => {
+          console.log(searchTerm);
+          const filteredAutos = autos.filter((auto) =>
+            auto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+          setSearchResults(filteredAutos);
+        };
 
-          const handleSearch = (searchTerm) => {
-            console.log(searchTerm);
-            const filteredAutos = autos.filter((auto) =>
-              auto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
-            );
-            setSearchResults(filteredAutos);
-          };
 
-          useEffect(() => {
-            const fetchAuto = async () => {
-              try {
-                const response = await axios.get("http://localhost:5000/autos");
-                const data = response.data;
-                setAutos(data);
-                setShuffledAutos(shuffleArray(data)); // Mezclar el arreglo al cargar los datos
-              } catch (error) {
-                console.error("Error fetching auto:", error);
-              }
-            };
-          
-            fetchAuto();
-          }, [dispatchLike]);
-
-  return (
-    <>
-    <div>
-    <Navbar/>
-    </div>
-    <div>
-        <Buscador onSearch={handleSearch}/>
-    </div>
-    <div className={styles.containerCards}>
-      {searchResults.length > 0
-          ? searchResults.map((auto) => (
-              <Card key={auto.id} auto={auto} handelLike={handelLike} />
-            ))
-          : shuffledAutos.map((auto) => <Card key={auto.id} auto={auto} handelLike={handelLike}/>)}
-    </div>
-
-    </>
-  )
-}
-
-export default Home
+        return (
+          <>
+            <div>
+              <Navbar />
+            </div>
+            <div>
+              <Buscador onSearch={handleSearch} />
+            </div>
+              <div className={styles.containerCards}>
+                {searchResults.length > 0
+                    ? searchResults.map((auto) => (
+                        <Card key={auto.id} auto={auto} handelLike={handelLike} />
+                    ))
+                    : autos.map((auto) => (
+                        <Card key={auto.id} auto={auto} handelLike={handelLike} />
+                    ))}
+            </div>
+          </>
+        );
+      };
+      
+      export default Home;
