3beb4002e9719870f7deb2aa31316ad200470d49
Nicolopez93 <nicolopezarq@gmail.com>
2024-03-08 15:25:03 -0300

    borre carpeta de mas en admin

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/home/Home';
 import IniciarSesion from './components/pages/inicioSesion/InicioSesion';
 import CrearCuenta from './components/pages/crearCuenta/CrearCuenta';
-import Administrador from './components/pages/administrador/Administrador';
 import Detalle from './components/pages/detalle/Detalle';
 import FlotaDeAutos from './components/pages/flotaAutos/FlotaDeAutos';
 import FlotaDeCamioneta from './components/pages/flotaCamioneta/FlotaDeCamioneta';
@@ -18,7 +17,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/iniciarSesion" element={<IniciarSesion />} />
-          <Route path="/admin" element={<Administrador />} />
           <Route path="/PanelAdministrador" element={<PanelAdministrador />} />
           <Route path="/crearCuenta" element={<CrearCuenta />} />
           <Route path="/detalle/:id" element={<Detalle />} />
