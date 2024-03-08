130a59166bb33a5ff91530a28ad6d2622bb05264
Nicolopez93 <nicolopezarq@gmail.com>
2024-03-08 15:52:03 -0300

    lista usuarios terminada

--- vite-project/src/components/pages/listaDeProductos/ListaDeProductos.jsx
+++ vite-project/src/components/pages/listaDeProductos/ListaDeProductos.jsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import AgregarProducto from '../../common/agregarProducto/AgregarProducto';
 import Navbar from '../../common/navbar/Navbar';
 import TablaAdministrador from '../../common/tablaAdmin/TablaAdministrador';
+import './ListaDeProductos.css'
 const ListaDeProductos = () => {
 
     const [open, setOpen] = React.useState(false);
@@ -10,13 +11,15 @@ const ListaDeProductos = () => {
     const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Navbar/>
+    <>
+    <Navbar/>
+    <div className='panel-productos-container'>
+      <h1 className='panel-productos-title'>Lista De Productos</h1>
       <Button onClick={handleOpen}>Agregar Producto</Button>
-      <AgregarProducto open ={open} handleClose ={handleClose} />
+    <AgregarProducto open ={open} handleClose ={handleClose} />
       <TablaAdministrador/>
-      
     </div>
+    </>
   )
 }
 
