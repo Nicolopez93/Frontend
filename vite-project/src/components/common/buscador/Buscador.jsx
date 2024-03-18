50edb7fae023f276823799e5560014b6a6bcd67b
Martin Hernandez <martinhernandez83@gmail.com>
2024-03-18 19:09:23 -0300

    first commit - search

--- vite-project/src/components/common/buscador/Buscador.jsx
+++ vite-project/src/components/common/buscador/Buscador.jsx
@@ -1,29 +1,93 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import imgAuto from '../../../assets/renegade-jelly-2023-576x340-1-1.png'
 import './buscador.css'
-const Buscador = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const handleInputChange = (event) => {
-  setSearchTerm( event.target.value)
-  };
-  const handleSearch = () => {
-    // Llamar a la función de búsqueda pasando el término de búsqueda
-    onSearch(searchTerm);
-  };
+
+const Buscador = ({ onSearchResults }) => {
+  const [formData, setFormData] = useState({
+    auto: '',
+    fechaRetiro: '',
+    fechaDevolucion: '',
+  })
+
+  const [filteredAutos, setFilteredAutos] = useState([])
+
+  const buscarVehiculo = (vehiculo) => {
+    fetch(`http://localhost:3000/autos`)
+      .then((res) => res.json())
+      .then((data) => {
+
+        const autos = data.filter( a => a.marca.toLowerCase() === vehiculo.toLowerCase() || a.nombre.toLowerCase() === vehiculo.toLowerCase())
+        setFilteredAutos(autos)
+        onSearchResults(autos)
+        // console.log(autos);
+      })
+      .catch((error) => {
+        console.error('Error fetching autos:', error);
+      });
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // console.log(formData)
+    buscarVehiculo(formData.auto)
+    setFormData({
+      auto: '',
+      fechaRetiro: '',
+      fechaDevolucion: '',
+    })
+  }
+
+  const { auto, fechaDevolucion, fechaRetiro } = formData
 
   return (
-    <div className="buscador-container">
-       <div >
-          <input type="text" placeholder="Buscar autos..." value={searchTerm} onChange={handleInputChange}/>
-          <input type="date" placeholder="Fecha de Retiro"  />
-          <input type="date" placeholder="Fecha de Devolución" />
-          <button className="buscar-autos-button" onClick={handleSearch}>Buscar</button>
+    <div className='buscador-container'>
+      <form onSubmit={handleSubmit}>
+        <div className='select-container'>
+          <input
+            name='auto'
+            required
+            type='text'
+            placeholder='Buscar autos...'
+            value={auto}
+            onChange={handleChange}
+          />
+          <input
+            name='fechaRetiro'
+            required
+            type='date'
+            placeholder='Fecha de Retiro'
+            value={fechaRetiro}
+            onChange={handleChange}
+          />
+          <input
+            name='fechaDevolucion'
+            required 
+            type='date'
+            placeholder='Fecha de Devolución'
+            value={fechaDevolucion}
+            onChange={handleChange}
+          />
+          <button
+            type='submit'
+            className='buscar-autos-button'>
+            Buscar
+          </button>
         </div>
-      <div className="carNow-image-container">
-        <img src={imgAuto} alt='auto' className="auto-img" />
-        </div>
+      </form>
+      <img
+        src={imgAuto}
+        alt='auto'
+        className='auto-img'
+      />
     </div>
   )
 }
-
 export default Buscador
