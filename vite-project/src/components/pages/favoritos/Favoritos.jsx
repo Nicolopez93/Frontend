117c069d568139c045607c94c270810df0e4a174
Nicolopez93 <nicolopezarq@gmail.com>
2024-03-21 19:59:15 -0300

    ultimo

--- vite-project/src/components/pages/favoritos/Favoritos.jsx
+++ vite-project/src/components/pages/favoritos/Favoritos.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from '../../common/card/Card';
+import { Link } from 'react-router-dom';
 import './favoritos.css';
 
 const Favoritos = () => {
@@ -19,14 +20,17 @@ const Favoritos = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Favoritos</h1>
+    <>
+    <section className="detalle-section">
+    <Link className="detalle-volver-btn" to="/">Volver</Link>
+    <h2 className="detalle-title">Tus Vehiculos Favoritos</h2>
       <div className='fav-container'>
         {autos.map((auto) => (
           <Card key={auto.id} auto={auto}/>
         ))}
       </div>
-    </div>
+    </section>
+    </>
   );
 };
 
