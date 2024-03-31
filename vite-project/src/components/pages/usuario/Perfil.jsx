f9da460df1f46d2c7836dfb55159461a467ef2fd
Nicolopez93 <nicolopezarq@gmail.com>
2024-03-30 23:34:18 -0300

    usuario resuelto

--- vite-project/src/components/pages/usuario/Perfil.jsx
+++ vite-project/src/components/pages/usuario/Perfil.jsx
@@ -1,20 +1,35 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../auth/context/AuthContext';
+import './perfil.css';
+import Avatar from '@mui/material/Avatar';
 
 const Perfil = () => {
-
   const { user } = useContext(AuthContext);
 
+  const obtenerInicial = () => {
+    if (user && user.nombre) {
+      return user.nombre.charAt(0).toUpperCase();
+    }
+    return '';
+  };
+
   return (
-    <div>
-      <div>
-        <h2>Datos de Usuario</h2>
-        <ul>
-          <li>Nombre: {user.nombre}</li>
-          <li>Email: {user.email}</li>
-          <li>Rol de Usuario: {user.usuarioRol}</li>
+    <div >
+      <h2 className="perfil-heading">Datos de Usuario</h2>
+      <div className="perfil-container">
+        
+        <ul className="perfil-list">
+          <li className="perfil-item">Nombre: {user.nombre}</li>
+          <li className="perfil-item">Email: {user.email}</li>
+          <li className="perfil-item">Rol de Usuario: {user.usuarioRol}</li>
         </ul>
+        <div>
+      <Avatar sx={{ width: 200, height: 200,fontSize: 100 }} style={{ backgroundColor: 'orange' }}>
+      {obtenerInicial()}
+            </Avatar>
       </div>
+      </div>
+      
     </div>
   );
 };
