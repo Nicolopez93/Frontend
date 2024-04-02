1f5b0107faac097715e54411e964573b9fd948f7
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-01 23:41:06 -0300

    listo algunos conflictos

--- vite-project/src/router/AppRouters.jsx
+++ vite-project/src/router/AppRouters.jsx
@@ -1,69 +1,65 @@
-import { Routes, Route } from 'react-router-dom'
-import { LoginPage } from '../auth/pages/LoginPage'
-import { CardRouters } from '../components/routers/CardRouters'
+import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../auth/pages/LoginPage';
+import { CardRouters } from '../components/routers/CardRouters';
 import { PrivateRouter } from './PrivateRouter'
 import { PublicRouters } from './PublicRouters'
 import { RegisterPage } from '../auth/pages/RegisterPage'
-import PanelAdministrador from '../components/pages/panelAdministrador/PanelAdministrador'
-import ListaDeProductos from '../components/pages/listaDeProductos/ListaDeProductos'
-import ListaDeUsuarios from '../components/pages/listaDeUsuarios/ListaDeUsuarios'
-import ListaDeCaracteristicas from '../components/pages/listaDeCaracteristicas/ListaDeCaracteristicas'
-import { Reserva } from '../components/pages/reserva/Reserva'
+import PanelAdministrador from '../components/pages/panelAdministrador/PanelAdministrador';
+import ListaDeProductos from '../components/pages/listaDeProductos/ListaDeProductos';
+import ListaDeUsuarios from '../components/pages/listaDeUsuarios/ListaDeUsuarios';
+import ListaDeCaracteristicas from '../components/pages/listaDeCaracteristicas/ListaDeCaracteristicas';
 import ListaDeCategorias from '../components/pages/listaDeCategorias/ListaDeCategorias';
+import { Reserva } from '../components/pages/reserva/Reserva'
 
 export const AppRouters = () => {
-  return (
+  return(
     <>
       <Routes>
-        <Route
-          path='login'
-          element={
-            <PublicRouters>
-              <LoginPage />
-            </PublicRouters>
-          }
-        />
+        <Route path='login' element={
+          <PublicRouters>
+            <LoginPage/>
+          </PublicRouters>
+        }/>
 
-        <Route
-          path='register'
-          element={<RegisterPage />}
-        />
-        <Route
-          path='/*'
-          element={<CardRouters />}
-        />
+        <Route path='register' element={
+            <RegisterPage/>
+        }/>
+        <Route path='/*' element={<CardRouters/>}/>
+        
+        <Route path='/PanelAdministrador' element={
+          <PrivateRouter>
+            <PanelAdministrador/>
+          </PrivateRouter>
+        }/>
 
+        <Route path='/ListaDeProductos' element={
+          <PrivateRouter>
+            <ListaDeProductos/>
+          </PrivateRouter>
+        }/>
+        <Route path='/ListaDeUsuarios' element={
+          <PrivateRouter>
+            <ListaDeUsuarios/>
+          </PrivateRouter>
+        }/>
+
+        <Route path='/ListaDeCaracteristicas' element={
+          <PrivateRouter>
+            <ListaDeCaracteristicas/>
+          </PrivateRouter>
+        }/>
+
+        <Route path='ListaDeCategorias' element={
+          <PrivateRouter>
+            <ListaDeCategorias/>
+          </PrivateRouter>
+        }/>
+        
         <Route
-          path='/PanelAdministrador'
+          path='/reserva'
           element={
             <PrivateRouter>
-              <PanelAdministrador />
-            </PrivateRouter>
-          }
-        />
-
-        <Route
-          path='/ListaDeProductos'
-          element={
-            <PrivateRouter>
-              <ListaDeProductos />
-            </PrivateRouter>
-          }
-        />
-        <Route
-          path='/ListaDeUsuarios'
-          element={
-            <PrivateRouter>
-              <ListaDeUsuarios />
-            </PrivateRouter>
-          }
-        />
-
-        <Route
-          path='/ListaDeCaracteristicas'
-          element={
-            <PrivateRouter>
-              <ListaDeCaracteristicas />
+              <Reserva />
             </PrivateRouter>
           }
         />
@@ -72,3 +68,4 @@ export const AppRouters = () => {
     </>
   )
 }
+
