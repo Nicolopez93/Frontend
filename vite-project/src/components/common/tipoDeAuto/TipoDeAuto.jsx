86d82c64bc087d08e209d7ae59ab957ade280d1e
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-03 16:31:46 -0300

    estilos

--- vite-project/src/components/common/tipoDeAuto/TipoDeAuto.jsx
+++ vite-project/src/components/common/tipoDeAuto/TipoDeAuto.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './tipoDeAuto.css';
+
 
 const TipoDeAuto = () => {
     
 
     return (
         <>
-        <div className='tipoDeAuto-container'>
-            <h2 className='tipoDeAuto-titulo'>ALQUILER DE AUTOS EN LATINOAMÉRICA</h2>
-                <div className='tipoDeAuto-card-link'>
-                <Link to="/flotaDeAutos"><button>Autos</button>                    
-                </Link>
-                <Link to="/flotaDeCamioneta">
-                <button>Camionetas</button>
-                </Link>
-                </div>
+          <div className='bg-cover bg-center h-[63vh]' style={{backgroundImage: 'url("https://media.kasperskydaily.com/wp-content/uploads/sites/87/2015/05/05201736/rentacar-featured.jpg")', backgroundSize: 'cover'}}>
+            <h2 className="text-4xl font-bold text-center text-white">ALQUILER DE AUTOS EN LATINOAMÉRICA</h2>
+            <div className="flex flex-row  gap-4 justify-center items-stretch ">
+              <Link to="/flotaDeAutos">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Autos</button>
+              </Link>
+              <Link to="/flotaDeCamioneta">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Camionetas</button>
+              </Link>
             </div>
+          </div>
         </>
-    );
-};
+      );
+    }      
 
 export default TipoDeAuto;
--- vite-project/src/components/pages/favoritos/Favoritos.jsx
+++ vite-project/src/components/pages/favoritos/Favoritos.jsx
@@ -23,13 +23,13 @@ const Favoritos = () => {
 
   return (
     <>
-      <section className=" bg-[#f5f5f5] min-h-screen  ">
+      <section className=" bg-[#f5f5f5] min-h-[calc(100vh-110px)]   ">
         <div className='mx-[4vw] pt-8 '>
         <Link to="/">
           <Button>Volver</Button>
         </Link>
-        <h2 className="detalle-title my-8" >Tus Vehiculos Favoritos</h2>
-        <div className='flex gap-4 justify-between items-center w-full'>
+        <h2 className="text-4xl  font-bold my-8 text-center" >Tus Vehiculos Favoritos</h2>
+        <div className="flex flex-wrap gap-4 justify-between items-center w-full">
           {autos.map((auto) => (
             <Card key={auto.id} auto={auto} />
           ))}
