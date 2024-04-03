1dd7f5671b4dc258ef5ee751daa1b0197ac79cde
Nicolopez93 <nicolopezarq@gmail.com>
2024-04-03 19:48:25 -0300

    arreglamos css

--- vite-project/src/components/common/tipoDeAuto/TipoDeAuto.jsx
+++ vite-project/src/components/common/tipoDeAuto/TipoDeAuto.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const TipoDeAuto = () => {
-    
-
-    return (
-        <>
-          <div className='bg-cover bg-center h-[63vh]' style={{backgroundImage: 'url("https://media.kasperskydaily.com/wp-content/uploads/sites/87/2015/05/05201736/rentacar-featured.jpg")', backgroundSize: 'cover'}}>
-            <h2 className="text-4xl font-bold text-center text-white">ALQUILER DE AUTOS EN LATINOAMÉRICA</h2>
-            <div className="flex flex-row  gap-4 justify-center items-stretch ">
-              <Link to="/flotaDeAutos">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Autos</button>
-              </Link>
-              <Link to="/flotaDeCamioneta">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Camionetas</button>
-              </Link>
-            </div>
-          </div>
-        </>
-      );
-    }      
+  return (
+      <div
+        className="bg-cover bg-center h-[63vh] flex flex-col justify-center items-center bg-[url('https://media.kasperskydaily.com/wp-content/uploads/sites/87/2015/05/05201736/rentacar-featured.jpg')]"
+      >
+        <div className="h-[18rem]">
+        <h2 className="text-4xl font-bold text-center text-blue-600">
+          ALQUILER DE AUTOS EN LATINOAMÉRICA
+        </h2>
+        <div className="flex flex-row  gap-4 justify-center items-stretch ">
+          <Link to="/flotaDeAutos">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Autos
+            </button>
+          </Link>
+          <Link to="/flotaDeCamioneta">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Camionetas
+            </button>
+          </Link>
+        </div>
+      </div>
+      </div>
+  );
+};
 
 export default TipoDeAuto;
